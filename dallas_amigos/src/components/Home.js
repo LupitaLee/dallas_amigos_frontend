d69f096d695c7279922e743a2ae5fffb4cea4883
Lupita <lupitarivera8899@gmail.com>
2021-10-08 12:24:56 -0500

    create new Home and Navigation component

---
+++ dallas_amigos/src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import "./Home.css"
+
+export default class Home extends Component{
+    render(){
+        return(
+            <div className="Home">
+                <h1 className="Home-h">Bienbenidos a dallas Amigos  </h1>
+          </div>
+        )
+    }
+} 
