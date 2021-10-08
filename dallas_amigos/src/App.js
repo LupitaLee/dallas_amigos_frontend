45b851aa86c65a2962b733e0b133b46da5f8fa28
Lupita <lupitarivera8899@gmail.com>
2021-10-08 12:09:27 -0500

    import boostrap to index.js

--- dallas_amigos/src/App.js
+++ dallas_amigos/src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-   hi
-    </div>
-  );
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import './App.css';
+
+class App extends Component {
+
+  render(){
+
+    return (
+      <div className="App">
+     hi
+      </div>
+    );
+  }
+  
 }
 
 export default App;
